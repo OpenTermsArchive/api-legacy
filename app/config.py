e79aa2b2e91c540a79b665f59ca89051afe45034
Vincent Viers <vincent.viers@beta.gouv.fr>
2020-12-02 16:23:59 +0100

    feat: implement rate limiting

--- app/config.py
+++ app/config.py
@@ -1,2 +1,3 @@
 CGUS_DATASET_PATH = "./dataset/"
 DATASET_DATE_FORMAT = "%Y-%m-%d--%H-%M-%S"
+RATE_LIMIT = "15/minute"
--- app/main.py
+++ app/main.py
@@ -1,19 +1,28 @@
 from pathlib import Path
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 
-from config import CGUS_DATASET_PATH
+from config import CGUS_DATASET_PATH, RATE_LIMIT
 from dataset_parser import CGUsFirstOccurenceParser
 
+limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
 
 @app.get("/")
-async def index():
+@limiter.limit(RATE_LIMIT)
+async def index(request: Request):
     return RedirectResponse("/docs")
 
 @app.get("/first_occurence/v1/{term}")
-async def first_occurence(term: str):
+@limiter.limit(RATE_LIMIT)
+async def first_occurence(request: Request, term: str):
     parser = CGUsFirstOccurenceParser(Path(CGUS_DATASET_PATH), term)
     parser.run()
     return parser.to_dict()

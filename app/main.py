8950ad2a1e4ca3d4c332c978cb2ea4ae7da48ffe
Vincent Viers <vincent.viers@beta.gouv.fr>
2020-12-03 12:04:05 +0100

    feat: add BASE_PATH var depending on env

--- app/main.py
+++ app/main.py
@@ -6,11 +6,11 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 
-from config import CGUS_DATASET_PATH, RATE_LIMIT
+from config import CGUS_DATASET_PATH, RATE_LIMIT, BASE_PATH
 from dataset_parser import CGUsFirstOccurenceParser
 
 limiter = Limiter(key_func=get_remote_address)
-app = FastAPI()
+app = FastAPI(openapi_url=f"{BASE_PATH}/openapi.json")
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
@@ -18,9 +18,9 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 @app.get("/")
 @limiter.limit(RATE_LIMIT)
 async def index(request: Request):
-    return RedirectResponse("/docs")
+    return RedirectResponse(f"{BASE_PATH}/docs")
 
-@app.get("/first_occurence/v1/{term}")
+@app.get(f"{BASE_PATH}/first_occurence/v1/{term}")
 @limiter.limit(RATE_LIMIT)
 async def first_occurence(request: Request, term: str):
     parser = CGUsFirstOccurenceParser(Path(CGUS_DATASET_PATH), term)

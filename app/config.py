388b65af195348224a29ebb9011afdd003cae430
Clément Biron <clement.biron@gmail.com>
2021-02-08 16:01:53 +0100

    Change rate limit

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 CGUS_DATASET_PATH = "./dataset/"
 DATASET_DATE_FORMAT = "%Y-%m-%d--%H-%M-%S"
-RATE_LIMIT = "15/minute"
+RATE_LIMIT = "240/minute"
 
 CGUS_API_ENV = os.getenv("CGUS_API_ENV", "development")
 BASE_PATH = "/api/cgus" if CGUS_API_ENV == "production" else ""

d10a97db7da8c2d3cf485d4b169b4248130674f4
Clément Biron <clement.biron@gmail.com>
2021-02-11 23:04:22 +0100

    Change API route 😅

--- app/config.py
+++ app/config.py
@@ -5,4 +5,4 @@ DATASET_DATE_FORMAT = "%Y-%m-%d--%H-%M-%S"
 RATE_LIMIT = "10000/minute"
 
 CGUS_API_ENV = os.getenv("CGUS_API_ENV", "development")
-BASE_PATH = "/api/open-document-archive" if CGUS_API_ENV == "production" else ""
+BASE_PATH = "/api/open-terms-archive" if CGUS_API_ENV == "production" else ""

1dbe7ae51b63e34fc950e3d02ebf8dee43f81af5
Vincent Viers <vincent.viers@beta.gouv.fr>
2021-02-25 12:51:06 +0100

    clean: please the pylint gods + format with black

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+# pylint: disable=unused-argument
 from pathlib import Path
 
 from fastapi import FastAPI, Request, HTTPException
@@ -9,12 +10,15 @@ from slowapi.errors import RateLimitExceeded
 
 from config import CGUS_DATASET_PATH, RATE_LIMIT, BASE_PATH
 from data_finder import CGUsDataFinder
-from dataset_parser import CGUsFirstOccurenceParser, CGUsAllOccurencesParser, CGUsDataset
+from dataset_parser import (
+    CGUsFirstOccurenceParser,
+    CGUsAllOccurencesParser,
+    CGUsDataset,
+)
 from utils import parse_user_date
 
 limiter = Limiter(key_func=get_remote_address)
-app = FastAPI(openapi_url=f"{BASE_PATH}/openapi.json",
-              docs_url=f"{BASE_PATH}/docs")
+app = FastAPI(openapi_url=f"{BASE_PATH}/openapi.json", docs_url=f"{BASE_PATH}/docs")
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
@@ -28,55 +32,74 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get(f"{BASE_PATH}/")
 @limiter.limit(RATE_LIMIT)
 async def index(request: Request):
+    """
+    redirect index to documentation
+    """
     return RedirectResponse(f"{BASE_PATH}/docs")
 
 
 @app.get(f"{BASE_PATH}/first_occurence/v1/{{term}}")
 @limiter.limit(RATE_LIMIT)
 async def first_occurence(request: Request, term: str):
+<<<<<<< HEAD
     """
     Returns the date of first occurence of a given term for every (Service - Document Type) pair.
     Search for multiple terms by separating them with a comma (e.g. "rgpd,trackers,cookies").
     Search is case-insensitive. `false` is returned if the term is not found.
     """
+=======
+    """TODO"""
+>>>>>>> clean: please the pylint gods + format with black
     parser = CGUsFirstOccurenceParser(Path(CGUS_DATASET_PATH), term)
     parser.run()
     return parser.to_dict()
 
+
 @app.get(f"{BASE_PATH}/all_occurences/v1/{{term}}")
 @limiter.limit(RATE_LIMIT)
 async def all_occurence(request: Request, term: str):
+<<<<<<< HEAD
     """
     Returns whether a version in the dataset contains a given term.
     Search for multiple terms by separating them with a comma (e.g. "rgpd,trackers,cookies").
     Search is case-insensitive.
     """
+=======
+    """TODO"""
+>>>>>>> clean: please the pylint gods + format with black
     parser = CGUsAllOccurencesParser(Path(CGUS_DATASET_PATH), term)
     parser.run()
     return parser.to_dict()
+
 
 @app.get(f"{BASE_PATH}/list_services/v1/")
 @limiter.limit(RATE_LIMIT)
 async def list_services(request: Request, multiple_versions_only: bool = False):
     """
     Returns a JSON object with services as keys and a list of their available document types.
-    multiple_versions_only: filters out service-document pairs for which only one version is recorded
+    multiple_versions_only: filters out service-document pairs for which only 1 version is recorded
     """
     dataset = CGUsDataset(Path(CGUS_DATASET_PATH))
-    return dataset.list_all_services_doc_types(multiple_versions_only=multiple_versions_only)
-    
+    return dataset.list_all_services_doc_types(
+        multiple_versions_only=multiple_versions_only
+    )
+
+
 @app.get(f"{BASE_PATH}/get_version_at_date/v1/{{service}}/{{document_type}}/{{date}}")
 @limiter.limit(RATE_LIMIT)
-async def get_version_at_date(request: Request, service: str, document_type: str, date: str):
+async def get_version_at_date(
+    request: Request, service: str, document_type: str, date: str
+):
     """
     Returns a the version for a given service and a given document type as it was on a certain date.
 
     The expected date format is YYYY-MM-DD.
 
-    Example : 
+    Example :
     /get_version_at_date/v1/Facebook/Terms of Service/2020-08-13
 
     {
@@ -91,6 +114,7 @@ async def get_version_at_date(request: Request, service: str, document_type: str
     """
     try:
         finder = CGUsDataFinder(service, document_type)
+<<<<<<< HEAD
     except Exception as e:
         raise HTTPException(400, str(e))
     try:
@@ -98,3 +122,14 @@ async def get_version_at_date(request: Request, service: str, document_type: str
     except ValueError as e:
         raise HTTPException(400, f"Issue parsing date : {str(e)}. Expected format is YYYY-MM-DD.")
     return finder.get_version_at_date(parsed_date)
+=======
+    except Exception as exception:  # pylint: disable=broad-except
+        return {"error": str(exception)}
+    try:
+        parsed_date = parse_user_date(date)
+    except ValueError as exception:
+        return {
+            "error": f"Issue parsing date : {str(exception)}. Expected format is YYYY-MM-DD."
+        }
+    return finder.get_version_at_date(parsed_date)
+>>>>>>> clean: please the pylint gods + format with black

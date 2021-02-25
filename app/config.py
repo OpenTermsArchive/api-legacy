44524c7fb355331cdc94745e51bf6ebe0c8efee3
Vincent Viers <vincent.viers@beta.gouv.fr>
2021-02-25 15:27:05 +0100

    feat: implement preprod

--- app/config.py
+++ app/config.py
@@ -5,4 +5,9 @@ DATASET_DATE_FORMAT = "%Y-%m-%d--%H-%M-%S"
 RATE_LIMIT = "10000/minute"
 
 CGUS_API_ENV = os.getenv("CGUS_API_ENV", "development")
-BASE_PATH = "/api/open-terms-archive" if CGUS_API_ENV == "production" else ""
+ENV_MAPPING = {
+    "production": "/api/open-terms-archive",
+    "preproduction": "/api/open-terms-archive-preprod",
+    "development": "",
+}
+BASE_PATH = ENV_MAPPING.get("CGUS_API_ENV", "")

0c9b39a6d7f58a616e7f492508c279f10f24a9ae
Martin Ratinaud <martinratinaud@gmail.com>
2021-03-01 09:43:58 +0400

    fix: adapt variable name + add preproduction to deployment

--- app/config.py
+++ app/config.py
@@ -4,10 +4,10 @@ CGUS_DATASET_PATH = "./dataset/"
 DATASET_DATE_FORMAT = "%Y-%m-%d--%H-%M-%S"
 RATE_LIMIT = "10000/minute"
 
-CGUS_API_ENV = os.getenv("CGUS_API_ENV", "development")
+OTA_API_ENV = os.getenv("OTA_API_ENV", "development")
 ENV_MAPPING = {
     "production": "/api/open-terms-archive",
     "preproduction": "/api/open-terms-archive-preprod",
     "development": "",
 }
-BASE_PATH = ENV_MAPPING.get("CGUS_API_ENV", "")
+BASE_PATH = ENV_MAPPING.get("OTA_API_ENV", "")

295ac74a28e03875eca6fefc4bce0651d2014618
Vincent Viers <vincent.viers@beta.gouv.fr>
2021-04-01 19:58:30 +0200

    fix: correct dataset path

--- download_dataset.sh
+++ download_dataset.sh
@@ -2,5 +2,5 @@
 file_url=$(curl --silent "https://api.github.com/repos/ambanum/OpenTermsArchive-versions/releases/latest" | grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/')
 echo "updating" > latest_dataset.txt
 curl -LJS $file_url -o dataset.zip
-unzip -o dataset.zip && rm -rf dataset && mv dataset-* dataset
+unzip -o dataset.zip && rm -rf dataset && mv data/dataset-* dataset
 echo $file_url > latest_dataset.txt
